365e60aa08ac34e9ae52b9795f3889919109205d
Heather Taylor <heather@heysparkbox.com>
2021-03-22 22:33:39 -0400

    Comment out gsap work to see if that is causing the build issue

--- src/js/gsap.js
+++ src/js/gsap.js
@@ -1,41 +1,41 @@
-import { gsap } from "gsap";
+// import { gsap } from "gsap";
 
-// ScrollTo / smoothly scroll from navigation items to their section on the page
+// // ScrollTo / smoothly scroll from navigation items to their section on the page
 
-import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
-gsap.registerPlugin(ScrollToPlugin);
+// import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
+// gsap.registerPlugin(ScrollToPlugin);
 
-let navButtons = ['discover', 'about', 'sell'];
+// let navButtons = ['discover', 'about', 'sell'];
 
-for (let i = 0; i < navButtons.length; i+=1) {
+// for (let i = 0; i < navButtons.length; i+=1) {
 
-  let id = `${navButtons[i] + "Btn"}`;
-  let anchor = document.getElementById(id);
-  let section = `#${navButtons[i]}`;
-  let timing = i + 1;
+//   let id = `${navButtons[i] + "Btn"}`;
+//   let anchor = document.getElementById(id);
+//   let section = `#${navButtons[i]}`;
+//   let timing = i + 1;
 
-  anchor.addEventListener("click", () => {
-    gsap.to(window, {
-      duration: timing,
-      ease: "circ",
-      scrollTo: {
-        y: section,
-        offsetY: -20,
-      },
-    });
-  });
-}
+//   anchor.addEventListener("click", () => {
+//     gsap.to(window, {
+//       duration: timing,
+//       ease: "circ",
+//       scrollTo: {
+//         y: section,
+//         offsetY: -20,
+//       },
+//     });
+//   });
+// }
 
-// Adds ScrollTo / smoothly scroll from an item clicked on in Products to the Where to Buy section
-document.querySelectorAll(".c-products__btn").forEach((buyBtn) => {
-  buyBtn.addEventListener("click", () => {
-    gsap.to(window, {
-      duration: 2,
-      ease: "circ",
-      scrollTo: { 
-        y: "#whereToBuy",
-        offsetY: -20,
-       },
-    });
-  });
-});
+// // Adds ScrollTo / smoothly scroll from an item clicked on in Products to the Where to Buy section
+// document.querySelectorAll(".c-products__btn").forEach((buyBtn) => {
+//   buyBtn.addEventListener("click", () => {
+//     gsap.to(window, {
+//       duration: 2,
+//       ease: "circ",
+//       scrollTo: { 
+//         y: "#whereToBuy",
+//         offsetY: -20,
+//        },
+//     });
+//   });
+// });
